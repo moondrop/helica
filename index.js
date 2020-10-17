7bf06d2a65bea3e88efc5218b5221dc246249ec0
Riya <kato.hana@outlook.jp>
2020-10-18 01:04:15 +0200

    Added an HTML specific render helper and removed unnecessary Number conversion

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ module.exports = {
      * Methods
      */
     send: require('./lib/helpers/EndResponse').send,
+    render: require('./lib/helpers/EndResponse').render,
 
     /**
      * Middlewares
