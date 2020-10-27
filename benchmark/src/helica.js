1bd32312d5185e63688b9573423384158be3bb37
Alex Taxiera <alex.taxiera@onesky.com>
2020-10-27 12:20:44 -0400

    change parameter array to parameter object

--- benchmark/src/helica.js
+++ benchmark/src/helica.js
@@ -2,12 +2,12 @@ const Helica = require('helica');
 const app = new Helica.Server({ sslApp: false, debug: false });
 
 app.addMiddleware(async (response, request) => {
-    request.double = Number(request.parameters[0]) * 2;
+    request.double = Number(request.parameters.number) * 2;
 });
 
 class RandomNumber {
     get(response, request) {
-        response.end(`Number: ${request.parameters[0]} | Double: ${request.double}`);
+        response.end(`Number: ${request.parameters.number} | Double: ${request.double}`);
     }
 }
 
